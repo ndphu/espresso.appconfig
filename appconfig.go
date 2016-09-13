3352fac6180fee24209b0f3ca99eb974e381567d
Phu Nguyen <ndphu@localhost.com>
2016-09-13 12:01:18 +0700

    use firebase auth

--- appconfig.go
+++ appconfig.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"github.com/ndphu/espresso.helper.firebase"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
 	"log"
@@ -30,11 +31,11 @@ type AppConfig struct {
 }
 
 var (
-	confFile  = kingpin.Flag("config-file", "Config file").String()
-	authToken = kingpin.Flag("token", "Firebase auth token").String()
-	logFile   = kingpin.Flag("log-file", "The path to the log file").String()
-	deviceId  = kingpin.Flag("device-id", "Required to use online config").String()
-	skipSSL   = kingpin.Flag("skip-ssl", "Skip SSL verification. Should be used only in test mode").Bool()
+	confFile = kingpin.Flag("config-file", "Config file").String()
+	keyFile  = kingpin.Flag("key-file", "Google OAuth key file").String()
+	logFile  = kingpin.Flag("log-file", "The path to the log file").String()
+	deviceId = kingpin.Flag("device-id", "Required to use online config").String()
+	skipSSL  = kingpin.Flag("skip-ssl", "Skip SSL verification. Should be used only in test mode").Bool()
 )
 
 func New() *AppConfig {
@@ -65,42 +66,36 @@ func (appConfig *AppConfig) Load() {
 		if *deviceId == "" {
 			panic("Device ID is not defined")
 		}
-		appConfig.GetConfigFromFirebase(*deviceId, *authToken)
+		if *keyFile == "" {
+			panic("Key file is not defined")
+		}
+		appConfig.GetConfigFromFirebase(*deviceId, *keyFile)
 	} else {
 		log.Printf("Using config file: %s\n", *confFile)
 		appConfig.ParseConfigFile(*confFile)
 	}
 }
 
-func (appConfig *AppConfig) GetConfigFromFirebase(deviceId string, authToken string) {
+func (appConfig *AppConfig) GetConfigFromFirebase(deviceId string, keyFile string) {
 	log.Println("Get config for device id =", deviceId, "from Firebase")
-	var client *http.Client
+	client := firebase_helper.NewFirebaseClient(keyFile)
 	if *skipSSL {
 		tr := &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
-		client = &http.Client{Transport: tr}
-	} else {
-		client = &http.Client{}
+		client.Transport = tr
 	}
+	body := firebase_helper.GetData(fmt.Sprintf("https://rpictl.firebaseio.com/config/%s", deviceId), client)
 
-	resp, err := client.Get(fmt.Sprintf("https://rpictl.firebaseio.com/config/%s.json?access_token=%s", deviceId, authToken))
-	if err != nil {
-		// handle error
-		log.Print("Error:", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 	if fmt.Sprintf("%s", body) == "null" {
 		log.Panicf("Cannot get online config for device %s", deviceId)
 	}
-	if err != nil {
-		// handle error
-		log.Print("Error:", err)
-		os.Exit(1)
-	}
-	appConfig.ParseConfig(body)
+	// if err != nil {
+	// 	// handle error
+	// 	log.Print("Error:", err)
+	// 	os.Exit(1)
+	// }
+	appConfig.ParseConfig([]byte(body))
 }
 
 func (appConfig *AppConfig) ParseConfigFile(confPath string) {
